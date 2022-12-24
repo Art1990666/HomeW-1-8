55d8f635dbe81b0111a359423abdc2c2c252d799
DNS-User <gulikyan-arthurAG@mail.ru>
2022-12-25 02:28:27 +0900

    HW4

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
     transient3();
     transient4();
     transient5();
+    transient6();
+    transient7();
+    transient8();
 }
     public static void transient1 () {
             System.out.println("work-1");
@@ -59,5 +62,31 @@ public class Main {
         System.out.println(frog);
         frog = frog +4;
         System.out.println(frog);
+    }
+    public static void transient6 () {
+        System.out.println("work-6");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var capacityLeft = boxer1 + boxer2;
+        System.out.println("Общий вес двух бойцов" + capacityLeft + "кг");
+    }
+    public static void transient7 () {
+        System.out.println("work-7");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var capacityLeft = boxer2 - boxer1;
+        System.out.println("Вычислите разницу весов спортсменов" + capacityLeft + "кг!");
+        var overLoad = boxer2 % boxer1;
+        System.out.println("Вычислите разницу весов спортсменов" + overLoad + "кг!");
+    }
+    public static void transient8 () {
+        System.out.println("work-8");
+        var totalHours = 640;
+        var forOne = 8;
+        var capacityLeft = totalHours / forOne;
+        System.out.println("Всего работников в компании" + capacityLeft );
+        capacityLeft = capacityLeft +94;
+        forOne = totalHours / capacityLeft;
+        System.out.println("Если в компании работает" + " " + capacityLeft + " " + "человек, то всего" + " " + forOne + " " + " часов работы может быть поделено между сотрудниками");
     }
 }
