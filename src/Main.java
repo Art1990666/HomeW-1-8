846b1a6f2377f768a87b488c2692e0291b4cd534
DNS-User <gulikyan-arthurAG@mail.ru>
2022-12-24 23:55:03 +0900

    HW3

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+    transient1();
+    transient2();
+    transient3();
+}
+    public static void transient1 () {
+            System.out.println("work-1");
+            var dog = 8;
+            System.out.println(dog);
+            var cat = 3.6;
+            System.out.println(cat);
+            var paper = 763789;
+            System.out.println(paper);
+
+    }
+    public static void transient2 () {
+        System.out.println("work-2");
+        var dog = 8;
+        dog = dog +4;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat +4;
+        System.out.println(cat);
+        var paper = 763789;
+        paper = paper +4;
+        System.out.println(paper);
+    }
+    public static void transient3 () {
+        System.out.println("work-3");
+        var dog = 8.0;
+        dog = dog -3.5;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat -1.6;
+        System.out.println(cat);
+        var paper = 763789;
+        paper = paper -7639;
+        System.out.println(paper);
+    }
+}
